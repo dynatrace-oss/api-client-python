d4644bf468ca4a1ed55ff725666c350515dc5a1d
dlopes7 <davidribeirolopes@gmail.com>
2020-11-10 07:28:41 -0300

    Version 1.014
    Fix totalStepCount in report_simple_thirdparty_synthetic_test
    Use a requests version that is compatible with Dynatrace Activegates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dtapi",
-    version="1.0.13",
+    version="1.0.14",
     packages=find_packages(),
     install_requires=["requests>=2.21"],
     python_requires=">=3.6",
