3a2c97fa67f78554d6d1252afd9e3f3848ebe134
dlopes7 <davidribeirolopes@gmail.com>
2021-05-01 14:44:57 -0300

    Version 1.1.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dtapi",
-    version="1.1.17",
+    version="1.1.18",
     packages=find_packages(),
     install_requires=["requests>=2.21"],
     tests_require=["pytest", "mock", "tox"],
