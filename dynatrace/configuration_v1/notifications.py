11370cbb7e74025b83906780c038d3f13e91410a
James Kitson <jpw.kitson@gmail.com>
2021-05-04 13:10:51 -0400

    webhook and http header notification objects

--- dynatrace/configuration_v1/notifications.py
+++ dynatrace/configuration_v1/notifications.py
@@ -40,6 +40,24 @@ class SlackNotificationConfig(Notification):
         self.channel: str = raw_element.get("channel")
         self.title: str = raw_element.get("title")
 
+class HttpHeader(DynatraceObject):
+    def _create_from_raw_data(self, raw_element):
+        self.name: str = raw_element.get("name")
+        self.value: str = raw_element.get("value")
+
+class WebHookNotificationConfig(Notification):
+    def _create_from_raw_data(self, raw_element):
+        self.id: str = raw_element.get("id")
+        self.name: str = raw_element.get("name")
+        self.notification_type: str = raw_element.get("type")
+        self.alerting_profile: AlertingProfile = AlertingProfile(raw_element=self._http_client.make_request(f"/api/config/v1/alertingProfiles/{raw_element.get('alertingProfile')}").json())
+        self.active: bool = raw_element.get("active")
+        self.url: str = raw_element.get("url")
+        self.accept_any_certificate: bool = raw_element.get("acceptAnyCertificate")
+        self.payload: str = raw_element.get("payload")
+        self.headers: List[HttpHeader] = [HttpHeader(raw_element=header) for header in raw_element.get("headers", {})]
+        self.notify_event_merges_enabled: bool = raw_element.get("notifyEventMergesEnabled")
+
 
 class NotificationConfigStub(DynatraceObject):
     def get_full_configuration(self) -> Notification:
@@ -51,6 +69,8 @@ class NotificationConfigStub(DynatraceObject):
             notification = EmailNotificationConfig(self._http_client, None, response)
         if self.notification_type == "SLACK":
             notification = SlackNotificationConfig(self._http_client, None, response)
+        if self.notification_type == "WEBHOOK":
+            notification = WebHookNotificationConfig(self._http_client, None, response)
         else:
             notification = Notification(self._http_client, None, response)
         return notification
