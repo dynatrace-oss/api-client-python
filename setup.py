34ceaf1bd7c1a8b42e10796deb009e6211769880
dlopes7 <davidribeirolopes@gmail.com>
2021-05-14 19:50:46 -0300

    Version 1.1.23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dtapi",
-    version="1.1.22",
+    version="1.1.23",
     packages=find_packages(),
     install_requires=["requests>=2.21"],
     tests_require=["pytest", "mock", "tox"],
