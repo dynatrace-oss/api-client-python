8001be9e57f60517e109b63d908f14d74c609d8b
James Kitson <jpw.kitson@gmail.com>
2021-05-14 10:56:00 -0400

    add MonitoringMethod enum

--- dynatrace/configuration_v1/anomaly_detection_process_groups.py
+++ dynatrace/configuration_v1/anomaly_detection_process_groups.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Dict, Any
+from enum import Enum
 from requests import Response
 
 from dynatrace.environment_v2.schemas import ConfigurationMetadata
@@ -7,9 +8,14 @@ from dynatrace.pagination import PaginatedList
 from dynatrace.dynatrace_object import DynatraceObject
 from dynatrace.http_client import HttpClient
 
+class MonitoringMethod(Enum):
+    OFF = "OFF"
+    MINIMUM_THRESHOLD = "MINIMUM_THRESHOLD"
+    PROCESS_IMPACT = "PROCESS_IMPACT"
+
 class AvailabilityMonitoringPG(DynatraceObject):
     def _create_from_raw_data(self, raw_element: Dict[str, Any]):
-        self.method: str = raw_element.get("method")
+        self.method: MonitoringMethod = MonitoringMethod(raw_element.get("method"))
         self.minimum_threshold: int = raw_element.get("minimumThreshold") 
 
 class AnomalyDetectionPG(DynatraceObject):
