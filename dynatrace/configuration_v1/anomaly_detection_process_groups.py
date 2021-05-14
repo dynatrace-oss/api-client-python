b99d9b8b65b9c2ca36c5bfa6990decbbc7de779b
James Kitson <jpw.kitson@gmail.com>
2021-05-14 11:45:56 -0400

    static method to create AnomalyDetectionPG config

--- dynatrace/configuration_v1/anomaly_detection_process_groups.py
+++ dynatrace/configuration_v1/anomaly_detection_process_groups.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Dict, Any
+from typing import List, Optional, Dict, Any, Union
 from enum import Enum
 from requests import Response
 
@@ -8,17 +8,26 @@ from dynatrace.pagination import PaginatedList
 from dynatrace.dynatrace_object import DynatraceObject
 from dynatrace.http_client import HttpClient
 
-class MonitoringMethod(Enum):
+class Method(Enum):
     OFF = "OFF"
     MINIMUM_THRESHOLD = "MINIMUM_THRESHOLD"
     PROCESS_IMPACT = "PROCESS_IMPACT"
 
 class AvailabilityMonitoringPG(DynatraceObject):
     def _create_from_raw_data(self, raw_element: Dict[str, Any]):
-        self.method: MonitoringMethod = MonitoringMethod(raw_element.get("method"))
+        self.method: Method = Method(raw_element.get("method"))
         self.minimum_threshold: int = raw_element.get("minimumThreshold") 
 
 class AnomalyDetectionPG(DynatraceObject):
+    @staticmethod
+    def create(method: Union[str, Method], minimum_threshold: Optional[int] = None):
+        method = Method(method)
+        if method == Method.MINIMUM_THRESHOLD:
+            raw_element = {"availabilityMonitoring": {"method": method.value, "minimum_threshold": minimum_threshold}}
+        else:
+            raw_element = {"availabilityMonitoring": {"method": method.value}}
+        return AnomalyDetectionPG(raw_element=raw_element)
+
     def _create_from_raw_data(self, raw_element: Dict[str, Any]):
         self.availability_monitoring: AvailabilityMonitoringPG = AvailabilityMonitoringPG(raw_element=raw_element.get("availabilityMonitoring"))
 
@@ -32,14 +41,15 @@ class AnomalyDetectionPGService:
         """
         return AnomalyDetectionPG(raw_element=self.__http_client.make_request(f"/api/config/v1/anomalyDetection/processGroups/{id}").json())
 
-    def update_configuration(self, id: str, method: str, minimum_threshold: Optional[int] = None):
+    def update_configuration(self, id: str, method: Union[Method, str], minimum_threshold: Optional[int] = None):
         """
         Update the anomaly detection configuration for the specified process group.
         """
-        if method == "MINIMUM_THRESHOLD":
-            body = {"availabilityMonitoring": {"method": method, "minimumThreshold": minimum_threshold}}
+        method = Method(method)
+        if method == Method.MINIMUM_THRESHOLD:
+            body = {"availabilityMonitoring": {"method": method.value, "minimumThreshold": minimum_threshold}}
         else:
-            body = {"availabilityMonitoring": {"method": method}}
+            body = {"availabilityMonitoring": {"method": method.value}}
         return self.__http_client.make_request(f"/api/config/v1/anomalyDetection/processGroups/{id}", method="PUT", params=body)
 
     def delete_configuration(self, id:str):
