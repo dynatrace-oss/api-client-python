8a3b32198d096f9753d66529af58c2f24adf6337
Hashmi <bilal.hashmi@dynatrace.com>
2021-06-09 15:50:38 -0500

    adding some more tests

--- test/environment_v2/test_networkzones.py
+++ test/environment_v2/test_networkzones.py
@@ -9,7 +9,9 @@ NETWORKZONE_ID = "default"
 def test_list(dt: Dynatrace):
     network_zones = dt.network_zones.list()
 
+    # type checks 
     assert isinstance(network_zones, PaginatedList)
+    assert len(list(network_zones)) == 2
     assert all(isinstance(n , nz.NetworkZone) for n in network_zones)
 
 
@@ -19,3 +21,11 @@ def test_get(dt: Dynatrace):
     # type checks
     assert isinstance(network_zone, nz.NetworkZone)
     assert isinstance(network_zone.alternative_zones, list)
+
+    # value checks
+    assert network_zone.description == "The default network zone. This is the network zone for OneAgents or ActiveGates that do not have any network zone configured."
+    assert network_zone.id == "default"
+    assert network_zone.num_configured_activegates == 0
+    assert network_zone.num_oneagents_configured == 141
+    assert network_zone.num_oneagents_using == 141
+    assert network_zone.num_oneagents_from_other_zones == 0
