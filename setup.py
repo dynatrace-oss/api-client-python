7b32e36d1d531c7fed0a2c848dc83d47cf6aef91
dlopes7 <davidribeirolopes@gmail.com>
2021-11-04 12:53:18 -0300

    Version 1.1.41
    Fixes optional entity name field

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dt",
-    version="1.1.40",
+    version="1.1.41",
     packages=find_packages(),
     install_requires=["requests>=2.22"],
     tests_require=["pytest", "mock", "tox"],
