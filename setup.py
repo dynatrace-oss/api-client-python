e07a45db346c60906d2015f77bc3593a297b053a
James Kitson <30292059+Dynatrace-James-Kitson@users.noreply.github.com>
2023-02-13 10:38:48 -0500

    Increment version for publish
    Increment version for publish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dt",
-    version="1.1.56",
+    version="1.1.57",
     packages=find_packages(),
     install_requires=["requests>=2.22"],
     tests_require=["pytest", "mock", "tox"],
