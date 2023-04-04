c150ed1d63480f08b9458df0b105755366f4bdf4
James Kitson <james.kitson@dynatrace.com>
2023-04-04 15:47:28 -0400

    correct list problem comments path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dt",
-    version="1.1.58",
+    version="1.1.59",
     packages=find_packages(),
     install_requires=["requests>=2.22"],
     tests_require=["pytest", "mock", "tox"],
